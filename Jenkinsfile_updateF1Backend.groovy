b29fd55f0644e0602dff623f629cddf8490fc3d1
Shouvik <shouvik.dutta96@gmail.com>
2020-10-15 12:39:45 -0500

    commented out the slack notification

--- Jenkinsfile_updateF1Backend.groovy
+++ Jenkinsfile_updateF1Backend.groovy
@@ -26,11 +26,11 @@ pipeline {
             }
         }
     } 
-    post {
-        always{
-            slackSend channel: '#apim-garage-day',
-                color: COLOR_MAP[currentBuild.currentResult],
-                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}"
-        }
-    }
+    // post {
+    //     always{
+    //         slackSend channel: '#apim-garage-day',
+    //             color: COLOR_MAP[currentBuild.currentResult],
+    //             message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}"
+    //     }
+    // }
 }
