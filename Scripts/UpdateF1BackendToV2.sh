e325f17bb15ac18bdd075413e4e954736ac7e998
Shouvik <shouvik.dutta96@gmail.com>
2020-10-21 15:25:01 -0500

    updated final fail check

--- Scripts/UpdateF1BackendToV2.sh
+++ Scripts/UpdateF1BackendToV2.sh
@@ -2,10 +2,10 @@ echo "Running Create Artifacts Script"
 sed "s/controller_host/$1/;s/controller_username/$2/;s/controller_password/$3/" ./PostmanCollections/Controller3.x_Env.postman_environment.json > temp_env.json
 
 newman run -k ./PostmanCollections/3_Controller3.x_deploy_F1_V2.postman_collection.json -e ./temp_env.json --bail
-if [ $? -eq 0 ]; then
-    rm ./temp_env.json
+newman_output=$?
+rm ./temp_env.json
+if [ $newman_output -eq 0 ]; then
     exit 0
 else
-    rm ./temp_env.json
     exit 1
 fi
